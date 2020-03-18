dc902aec9b1d437eb3778c0bc2f8102ad56246c0
Doug Jones <dsjr1284@gmail.com>
2020-03-18 21:34:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,15 @@ class Dog
   def bark
     puts "woof!"
   end 
+ 
+
+def name (dog_name)
+  @name= dog_name 
 end 
 
-# def name (dog_name)
-#   @name= dog_name 
-# end 
 
+def name
+  @name 
+end 
 
-# def name
-#   @name 
-# end 
-
-
+end 
